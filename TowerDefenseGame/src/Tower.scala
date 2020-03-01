05ac25fce28e7e001ed8fa992e13739e747087b9
Aleksi Hirvensalo <aleksi.hirvensalo@aalto.fi>
2020-03-01 22:41:26 +0200

    Torni-luokka lisätty

--- TowerDefenseGame/src/Tower.scala
+++ TowerDefenseGame/src/Tower.scala
@@ -1,5 +1,28 @@
 
 
-abstract class Tower {
+abstract class Tower(val name: String, LP: Int, coordinates: (Int, Int), price: Int) {
+  
+  private var lifepoints = LP
+  
+  var coords = coordinates
+  
+  //Tämä kertoo hinnan, joka saadaan jos torni myydään
+  private var priceToSell = price * 0.8
+  
+  def getSellPrice = priceToSell
+  
+  def health = lifepoints
+  
+  def changeCoords(x: Int, y: Int) = coords = (x, y)
+  
+  def lowerHealth(amount: Int) = lifepoints - amount
+  
+  def increaseHealth(amount: Int) = lifepoints + amount
+  
+  def isAlive = lifepoints > 0 
+  
+  def act(): Unit
+  
+  
   
 }
